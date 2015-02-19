b387eba77379c85e5c62007cdb99d3afeec99ae9
Eric Clemmons <eric@smarterspam.com>
2015-02-19 17:56:05 -0600

    State works somehow?

--- examples/contacts/handlers/app.js
+++ examples/contacts/handlers/app.js
@@ -7,21 +7,37 @@ var RouteHandler  = require('react-router').RouteHandler;
 var App = React.createClass({
   mixins: [Resolver.mixin],
 
-  statics: {
-    resolve: {
-      loggedIn: function() {
-        return true;
-      }
+  getInitialState: function() {
+    return {
+      loggedIn: (typeof window === 'undefined') ? true : false
     }
+  },
+
+  login: function() {
+    this.setState({ loggedIn: true });
+  },
+
+  logout: function() {
+    this.setState({ loggedIn: false });
   },
 
   render() {
     return (
       <section>
         <Header />
-        <RouteHandler />
+        {this.state.loggedIn ? <RouteHandler /> : this.renderLogin()}
         <Footer />
       </section>
+    );
+  },
+
+  renderLogin() {
+    return (
+      <p className="text-center">
+        <a href="javascript:void(0)" onClick={this.login} className="btn btn-success btn-lg">
+          Login
+        </a>
+      </p>
     );
   }
 });
