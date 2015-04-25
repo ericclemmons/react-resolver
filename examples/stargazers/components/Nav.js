882a4157372fc5d7e16c22bc51788f2a77cc0d86
Eric Clemmons <eric@smarterspam.com>
2015-04-25 14:20:27 -0500

    Name links to home

--- examples/stargazers/components/Nav.js
+++ examples/stargazers/components/Nav.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import React from "react";
 
 export default class Nav extends React.Component {
@@ -6,7 +7,7 @@ export default class Nav extends React.Component {
       <nav className="deep-purple darken-2">
         <div className="nav-wrapper">
           <div className="container">
-            <a href="https://github.com/ericclemmons/react-resolver" className="brand-logo">React Resolver Demo</a>
+            <Link to="home" className="brand-logo">React Resolver Demo</Link>
             <ul className="right hide-on-small-only">
               <li>
                 <iframe src="https://ghbtns.com/github-btn.html?user=ericclemmons&type=follow&count=true" frameBorder="0" scrolling="0" width="150px" height="20px"></iframe>
