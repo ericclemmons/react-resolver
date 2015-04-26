4154749ecbd96ea04d650d2841cceacde38f8dad
Eric Clemmons <eric@smarterspam.com>
2015-04-25 19:00:05 -0500

    Move Follow from Nav to Stargazers

--- examples/stargazers/components/Nav.js
+++ examples/stargazers/components/Nav.js
@@ -8,11 +8,6 @@ export default class Nav extends React.Component {
         <div className="nav-wrapper">
           <div className="container">
             <Link to="home" className="brand-logo">React Resolver Demo</Link>
-            <ul className="right hide-on-small-only">
-              <li>
-                <iframe src="https://ghbtns.com/github-btn.html?user=ericclemmons&type=follow&count=true" frameBorder="0" scrolling="0" width="150px" height="20px"></iframe>
-              </li>
-            </ul>
           </div>
         </div>
       </nav>
