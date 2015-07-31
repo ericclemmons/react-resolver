5cccaabbc34e12c333911578f0644fc427d17706
Eric Clemmons <eric@smarterspam.com>
2015-07-31 13:40:50 -0500

    Alphabetize

--- examples/react-router/client.js
+++ examples/react-router/client.js
@@ -1,8 +1,8 @@
+import ES6Promise from "es6-promise";
 import { history } from "react-router/lib/HashHistory";
 import React from "react";
 import { render } from "react-dom";
 import Router from "react-router";
-import ES6Promise from "es6-promise";
 
 import routes from "./routes";
 
