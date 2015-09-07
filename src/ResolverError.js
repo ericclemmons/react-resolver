323ebd027b7b33f6250d09b73181af4c50f13f8d
Eric Clemmons <eric@smarterspam.com>
2015-09-07 17:41:05 -0500

    ResolveError supports an extra "data" argument

--- src/ResolverError.js
+++ src/ResolverError.js
@@ -1,9 +1,10 @@
 export default class ResolverError extends Error {
-  constructor(message) {
+  constructor(message, data) {
     super(message);
 
     this.name = this.constructor.name;
     this.message = message;
+    this.data = data;
 
     if (Error.hasOwnProperty("captureStackTrace")) {
       Error.captureStackTrace(this, this.constructor);
