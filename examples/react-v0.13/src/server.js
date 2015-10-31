31d416f86a641eac61333ffde699e0ee98dc1e09
Matthew Keas <matthiasak@gmail.com>
2015-10-31 11:37:52 -0500

    establish that this is simply a workaround until browserify can be better-configured to not inject duplicates into the build

--- examples/react-v0.13/src/server.js
+++
@@ -1,7 +0,0 @@
-require("babel-core/register")({ only: /src|react-resolver/ });
-
-var app = require("./app");
-
-app.listen(3000, function() {
-  console.info("✅  Node server started at http://localhost:3000/");
-});
